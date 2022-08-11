2d40305279c070af320d5fbd8c1e9f46fd13fe56
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-12 00:45:13 +0530

    added code

--- 4-redis.sh
+++ 4-redis.sh
@@ -1,8 +1,18 @@
+source common.sh
 
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>/tmp/roboshop.log
-yum install redis-6.2.7 -y &>>/tmp/roboshop.log
+COMPONENT=catalogu
+
+echo "set up yum repo"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+statuscheck
+
+echo Installing redis
+yum install redis-6.2.7 -y &>>${LOG}
+statuscheck
 
 #Update the bind from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
-systemctl enable redis &>>/tmp/roboshop.log
-systemctl start redis &>>/tmp/roboshop.log
+echo start Redis service
+systemctl enable redis &>>${LOG}
+systemctl restart redis &>>${LOG}
+statuscheck
