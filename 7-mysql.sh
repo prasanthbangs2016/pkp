71a9784cebcf13d6d904e1d8fefb332243c9306a
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-12 01:02:30 +0530

    added code

--- 7-mysql.sh
+++ 7-mysql.sh
@@ -1,21 +1,60 @@
+source common.sh
+
+COMPONENT=mysql
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+  echo -e "\e[33m env variable MYSQL_PASSWORD is missing \e[0m"
+  exit 1
+fi
+
 #**Manual Steps to Install MySQL**
 
 #As per the Application need, we are choosing MySQL 5.7 version.
 
 #Setup MySQL Repo
 
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/roboshop.log
 
-yum install mysql-community-server -y &>>/tmp/roboshop.log
+echo set up mysql repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+statuscheck
 
-systemctl enable mysqld &>>/tmp/roboshop.log
-systemctl start mysqld &>>/tmp/roboshop.log
+echo installing mysql
+yum install mysql-community-server -y &>>${LOG}
+statuscheck
+
+echo start mysql service
+systemctl enable mysqld &>>${LOG}
+systemctl start mysqld &>>${LOG}
+statuscheck
 
 #DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-DEFAULT_PASSWORD=$('A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+#if above commnand password is wrong we're changing the password
+if [ $? -ne 0 ]; then
+  echo Changing Default Password
+  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>${LOG}
+  StatusCheck
+fi
+
+echo "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD 2>&1 | grep validate_password &>>${LOG}
+#checking plugin is available or not if not available removing it
+if [ $? -eq 0 ]; then
+  echo Remove Password Validate Plugin
+  echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+  StatusCheck
+fi
+
+DOWNLOAD
+
+echo "Extract & Load Schema"
+cd /tmp &>>${LOG} && unzip -o mysql.zip &>>${LOG} &&  cd mysql-main &>>${LOG} && mysql -u root -p$MYSQL_PASSWORD <shipping.sql &>>${LOG}
+StatusCheck
+
+
 #Now a default root password will be generated and given in the log file.
 
-grep temp /var/log/mysqld.log 
+#grep temp /var/log/mysqld.log 
 
 #Next, We need to change the default root password in order to start using the database service. Use password RoboShop@1 or any other as per your choice. Rest of the options you can choose No
 
@@ -23,12 +62,12 @@ grep temp /var/log/mysqld.log
 #change mysql root password,first login to mysql and then change the password
 #https://stackoverflow.com/questions/7534056/mysql-root-password-change
 #ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your_pass_here';
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p$(DEFAULT_PASSWORD)
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p$(DEFAULT_PASSWORD)
 
 
 
 #You can check the new password working or not using the following command in MySQL
-echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1
+#echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1
 
 #mysql -uroot -pRoboShop@1
 
@@ -45,9 +84,4 @@ echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1
 
 #o download schema, Use the following command
 
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
 
-cd /tmp
-unzip -o mysql.zip
-cd mysql-main
-mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/roboshop.log
