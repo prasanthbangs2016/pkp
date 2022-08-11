bf203219f453846d5ef57e542c74dabb2f77ecba
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-12 03:28:53 +0530

    added code

--- 9-rabbitmq.sh
+++ 9-rabbitmq.sh
@@ -13,13 +13,16 @@ fi
 #RabbitMQ is a messaging Queue which is used by some components of the applications.
 
 #Erlang is a dependency which is needed for RabbitMQ
-echo install rabbite mq erlang from repo
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>${LOG}
-statuscheck
 
-echo installing rabbitmq
+echo installing erlang from repo
 curl -s https://packagecloud.io/install/repositories/${COMPONENT}/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
 statuscheck
+
+echo install rabbite mq erlang from repo
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG}
+statuscheck
+
+
 
 echo installing rabbitmq server
 yum install rabbitmq-server -y &>>${LOG}
