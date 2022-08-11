1d7b06af55357486c7d73ea9f2f765af5d70647f
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-12 04:03:11 +0530

    added code

--- 2-mongodb.sh
+++ 2-mongodb.sh
@@ -16,6 +16,9 @@ systemctl enable mongod &>>${LOG}
 systemctl start mongod &>>${LOG}
 statuscheck
 
+echo update mongodb listen address
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+statuscheck
 
 DOWNLOAD
 
