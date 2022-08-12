552c894eabaf558f08436efc2471f5f2c279f55e
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-13 01:18:46 +0530

    added files

--- common.sh
+++ common.sh
@@ -35,7 +35,7 @@ APP_CLEAN() {
 SYSTEMD() {
   echo Update SystemD Config
   sed -i -e 's/MONGO_DNSNAME/mongodb-dev.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb-dev.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-dev.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue-dev.roboshop.internal/' -e 's/AMQPHOST/rabbitmq-dev.roboshop.internal/' -e 's/CARTHOST/cart-dev.roboshop.internal/' -e 's/USERHOST/user-dev.roboshop.internal/' -e 's/CARTENDPOINT/cart-dev.roboshop.internal/' -e 's/DBHOST/mysql-dev.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
-  StatusCheck
+  statuscheck
 
   echo configuring ${COMPONENT} systemD service
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG}
