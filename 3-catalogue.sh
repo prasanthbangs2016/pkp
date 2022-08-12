d31159b969fab80d2f7bdb5f3bb3bac3947b9140
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-12 06:09:37 +0530

    added code

--- 3-catalogue.sh
+++ 3-catalogue.sh
@@ -3,8 +3,11 @@ source common.sh
 
 
 
+sed -i -e 's/MONGO_DNSNAME/mongodb-dev.roboshop.internal/' /etc/systemd/system/${COMPONENT}.service &>>${LOG}
+statuscheck
+
 NODEJS
-SYSTEMD
+
 
 
 
--- common.sh
+++ common.sh
@@ -34,13 +34,11 @@ APP_CLEAN() {
 
 SYSTEMD() {
 
-  echo update systemd config
-  sed -i -e 's/MONGO_DNSNAME/mongodb-dev.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb-dev.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-dev.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue-dev.roboshop.internal/' -e 's/AMQPHOST/rabbitmq-dev.roboshop.internal/' -e 's/CARTHOST/cart-dev.roboshop.internal/' -e 's/USERHOST/user-dev.roboshop.internal/' -e 's/CARTENDPOINT/cart-dev.roboshop.internal/' -e 's/DBHOST/mysql-dev.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG}
-  statuscheck
-
   echo configuring ${COMPONENT} systemD service
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG}
   systemctl daemon-reload &>>${LOG}
+  statuscheck
+
 
   echo starting ${COMPONENT} service
   systemctl restart ${COMPONENT} &>>${LOG}
